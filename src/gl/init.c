24a8535ddf004e02c7fbe0de9e15cbbd201eb162
The Judge <53906078+thejudge156@users.noreply.github.com>
2022-06-20 13:37:21 -0700

    Update init.c

--- src/gl/init.c
+++ src/gl/init.c
@@ -143,8 +143,7 @@ void initialize_gl4es() {
 		globals4es.es=ReturnEnvVarInt("LIBGL_ES");
     switch(globals4es.es) {
       case 1:
-      case 2:
-      case 3:	    
+      case 2:   
         break;
       default:
         // automatic ES backend selection
